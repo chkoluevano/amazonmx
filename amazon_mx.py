e72c6f3a150e8f75d5eaba6a1080d6c4045017fc
chko luevano <sluevano@gmail.com.com>
2020-06-06 18:33:32 -0500

    validate URL

--- amazon_mx.py
+++ amazon_mx.py
@@ -1,35 +1,43 @@
 import requests
 from bs4 import BeautifulSoup
 
+def isValid(mainURL):
+    if mainURL.startswith('https://www.amazon.com.mx/'):
+        return True
+    return False
+
 def getPrice(mainurl):
-    headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64;x64; rv:66.0) Gecko/20100101 Firefox/66.0", "Accept-Encoding":"gzip, deflate",     "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8", "DNT":"1","Connection":"close", "Upgrade-Insecure-Requests":"1"}
-    req = requests.get(mainurl, headers=headers)
-    soup = BeautifulSoup(req.text, "html.parser")
-    title = soup.find('span',id='productTitle')
-    price = soup.find('span', id = 'priceblock_ourprice')
-    if price is None:
-        price = soup.find('span', id = 'priceblock_saleprice')
-    titulo = title.contents[0].strip()
-    precio = price.contents[0]
-    currency = "MX"
-    f = open("price.txt",'r')
-    txtPrice = f.read()
-    f.close() 
-    if txtPrice != precio:
-        txtPrice = int(float(txtPrice[1:].replace(',','')))
-        newPrice = int(float(precio[1:].replace(',','')))
-        if txtPrice>newPrice:
-            d = txtPrice - newPrice
-            obs = f'El precio de {titulo} bajó ${d} pesos, su valor actual es {newPrice}'
+    if isValid(mainurl):
+        headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64;x64; rv:66.0) Gecko/20100101 Firefox/66.0", "Accept-Encoding":"gzip, deflate",     "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8", "DNT":"1","Connection":"close", "Upgrade-Insecure-Requests":"1"}
+        req = requests.get(mainurl, headers=headers)
+        soup = BeautifulSoup(req.text, "html.parser")
+        title = soup.find('span',id='productTitle')
+        price = soup.find('span', id = 'priceblock_ourprice')
+        if price is None:
+            price = soup.find('span', id = 'priceblock_saleprice')
+        titulo = title.contents[0].strip()
+        precio = price.contents[0]
+        currency = "MX"
+        f = open("price.txt",'r')
+        txtPrice = f.read()
+        f.close() 
+        if txtPrice != precio:
+            txtPrice = int(float(txtPrice[1:].replace(',','')))
+            newPrice = int(float(precio[1:].replace(',','')))
+            if txtPrice>newPrice:
+                d = txtPrice - newPrice
+                obs = f'El precio de {titulo} bajó ${d} pesos, su valor actual es {newPrice}'
+            else:
+                d = newPrice - txtPrice
+                obs = f'El precio de {titulo} subió ${d} pesos, su valor actual es {newPrice}'
+            f = open("price.txt",'w')
+            f.write(precio) 
+            f.close()
         else:
-            d = newPrice - txtPrice
-            obs = f'El precio de {titulo} subió ${d} pesos, su valor actual es {newPrice}'
-        f = open("price.txt",'w')
-        f.write(precio) 
-        f.close()
+            obs = f"EL precio de {titulo} se mantiene en {txtPrice}"
+        return obs
     else:
-        obs = f"EL precio de {titulo} se mantiene en {txtPrice}"
-    return obs
+        return "Invalid URL amazonMX "
 
 
 
--- test.py
+++ test.py
@@ -1,5 +1,5 @@
-from amazon_mx import getPrice
+import amazon_mx
 
 mainurl = 'https://www.amazon.com.mx/dp/B086GG3W24?ref=dacx_dp_7868297510501_4011434530001&me=AVDBXBAVVSXLQ&aaxitk=AD5KB6crxpEuiZD2AU6WZw'
-price = getPrice(mainurl)
+price = amazon_mx.getPrice(mainurl)
 print(price)
